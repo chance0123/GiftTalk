004435aa33471022c87367f1677dfb105b8ad6dd
cl_eof@163.com <abc0123>
2016-11-09 10:14:39 +0800

    初始导入(1)

---
+++ greendao2/src/main/java/com/example/MyClass.java
@@ -0,0 +1,13 @@
+package com.example;
+
+
+import javax.xml.validation.Schema;
+
+public class MyClass {
+    public static void main(String[] args) {
+        //创建一个模板
+        /**
+         * 参数一：当前模板的名称，会作为自动生成的java泪的e
+         */
+    }
+}
