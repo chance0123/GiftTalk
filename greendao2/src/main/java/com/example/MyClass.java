a9be7b0e5b764bb11f5c74b690adcba26ad0c791
cl_eof@163.com <abc0123>
2016-11-09 11:09:38 +0800

    初始导入(2)

--- greendao2/src/main/java/com/example/MyClass.java
+++ greendao2/src/main/java/com/example/MyClass.java
@@ -1,7 +1,9 @@
 package com.example;
 
 
-import javax.xml.validation.Schema;
+import org.greenrobot.greendao.generator.DaoGenerator;
+import org.greenrobot.greendao.generator.Entity;
+import org.greenrobot.greendao.generator.Schema;
 
 public class MyClass {
     public static void main(String[] args) {
@@ -9,5 +11,16 @@ public class MyClass {
         /**
          * 参数一：当前模板的名称，会作为自动生成的java泪的e
          */
+        Schema schema = new Schema("Android1607",1,"com.android.chengl.greendao.dao");
+        Entity entity = schema.addEntity("User");
+        entity.addIdProperty().primaryKey().autoincrement();
+        entity.addStringProperty("name").notNull();
+        entity.addStringProperty("sex");
+
+        try {
+            new DaoGenerator().generateAll(schema,"../GiftOne/grrndao/src/java");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
